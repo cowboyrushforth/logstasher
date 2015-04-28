fd8937df55e01187b491ee2c66c1aab8f50f3cf7
scott rushforth <scott@spotman.net>
2015-04-28 11:31:53 -0700

    add more fields to logging out put such as user-agent and more

--- logstasher.go
+++ logstasher.go
@@ -4,40 +4,46 @@ package logstasher
 
 import (
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"io"
 	"log"
+	"strconv"
 	"time"
-        "strconv"
-        "github.com/gin-gonic/gin"
 )
 
 type logstashEvent struct {
-	Timestamp string              `json:"@timestamp"`
-	Version   int                 `json:"@version"`
-	Method    string              `json:"method"`
-	Path      string              `json:"path"`
-	Status    int                 `json:"status"`
-	Size      int                 `json:"size"`
-	Duration  float64             `json:"duration"`
-	Params    map[string][]string `json:"params,omitempty"`
+	Timestamp     string              `json:"@timestamp"`
+	Version       int                 `json:"@version"`
+	Method        string              `json:"method"`
+	Path          string              `json:"path"`
+	Status        int                 `json:"status"`
+	Size          int                 `json:"size"`
+	Duration      float64             `json:"duration"`
+	Params        map[string][]string `json:"params,omitempty"`
+	UserAgent     string              `json:"user_agent"`
+	ContentLength string              `json:"content_length"`
+	ContentType   string              `json:"content_type"`
+	Referer       string              `json:"referer"`
+	XForward      string              `json:"xforward"`
+	Origin        string              `json:"origin"`
 }
 
 // Logger returns a middleware handler prints the request in a Logstash-JSON compatiable format
 func Logger(writer io.Writer) gin.HandlerFunc {
 	out := log.New(writer, "", 0)
-        return func(c *gin.Context) {
+	return func(c *gin.Context) {
 		start := time.Now()
 
 		rw := c.Writer
 		c.Next()
-                amount_written,_ := strconv.Atoi(rw.Header().Get("Content-Length")) 
-                params := make(map[string][]string)
+		amount_written, _ := strconv.Atoi(rw.Header().Get("Content-Length"))
+		params := make(map[string][]string)
 
-                // GET params
-                query_values := c.Request.URL.Query()
-                for k, v := range query_values {
-                  params[k] = v
-                }
+		// GET params
+		query_values := c.Request.URL.Query()
+		for k, v := range query_values {
+			params[k] = v
+		}
 
 		event := logstashEvent{
 			time.Now().Format(time.RFC3339),
@@ -47,7 +53,13 @@ func Logger(writer io.Writer) gin.HandlerFunc {
 			rw.Status(),
 			amount_written,
 			time.Since(start).Seconds() * 1000.0,
-                        params,
+			params,
+			c.Request.Header.Get("User-Agent"),
+			c.Request.Header.Get("Content-Length"),
+			c.Request.Header.Get("Content-Type"),
+			c.Request.Header.Get("Referer"),
+			c.Request.Header.Get("X-Forwarded-For"),
+			c.Request.Header.Get("Origin"),
 		}
 
 		output, err := json.Marshal(event)
